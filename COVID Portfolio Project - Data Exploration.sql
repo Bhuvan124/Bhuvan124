562243547658a3c955b79908e8997dfb2b2dd2f5
Bhuvanasai malapati <157451086+Bhuvan124@users.noreply.github.com>
2024-05-07 10:56:31 -0400

    Update COVID Portfolio Project - Data Exploration.sql

--- COVID Portfolio Project - Data Exploration.sql
+++ COVID Portfolio Project - Data Exploration.sql
@@ -1,9 +1,4 @@
-/*
-Covid 19 Data Exploration 
 
-Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
-
-*/
 
 Select *
 From PortfolioProject..CovidDeaths
